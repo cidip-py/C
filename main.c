fad1ee1c9faf9178e6420044388e2315ffac9817
cid <cidlorenzoapariciogarcia@gmail.com>
2022-04-27 20:28:04 -0300

    C calculator 1.1

--- main.c
+++ main.c
@@ -6,7 +6,14 @@ int main(){
     int num1;
     int num2;
     int inp;
+    char name[20];
     printf("Hello, we gonna do some maths!\n");
+    printf("Whats your name?\n");
+    fgets(name, 20,stdin);
+    if (name > 20)
+    {
+    printf("Please enter a valid name...");   
+       }
     printf("What do you wanna do?\n");
     printf(" 1)increment\n 2)decrement\n 3)multiplication\n 4)power\n");
     scanf("%d",&inp);
@@ -16,7 +23,7 @@ int main(){
         scanf("%d", &num1);
         printf("give me another number: \n");
         scanf("%d", &num2);
-        printf("The result of that operation is: %d", num1 + num2);
+        printf("The result of that operation is: %d, mr. %s", num1 + num2, name);
     }
     else if (inp == 2)
     {
@@ -24,7 +31,7 @@ int main(){
         scanf("%d", &num1);
         printf("give me another number: \n");
         scanf("%d", &num2);
-        printf("The result of that operation is: %d", num1 - num2);
+        printf("The result of that operation is: %d, mr. %s", num1 - num2, name);
     }
 
     else if (inp == 3)
@@ -33,7 +40,7 @@ int main(){
         scanf("%d", &num1);
         printf("give me another number: \n");
         scanf("%d", &num2);
-        printf("The result of that operation is: %d", num1 * num2);
+        printf("The result of that operation is: %d mr. %s", num1 * num2, name);
     }
     /*
     else if (inp == 4)
@@ -45,5 +52,10 @@ int main(){
         scanf("%d", &num2);
         printf("The result of %d ^ %d is: %d", num1, num2,  pow(num1, num2));
         }*/
+
+    else
+    {
+    printf("please select a valid operation...");
+        }
     return 0; 
 }
